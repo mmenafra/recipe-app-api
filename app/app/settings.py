4e45d30fcc0b0a405f69196b84e2289b7a64e209
Matias Menafra <mmenafra@gmail.com>
2021-12-28 16:59:22 -0300

    auth and tokens

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'users',
 ]
 
 MIDDLEWARE = [
