3136218507fae333bfec9ffda04ea2bd7d63dafc
Matias Menafra <mmenafra@gmail.com>
2021-12-29 11:40:48 -0300

    tag funcionality

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+def sample_user(email='test@london.com', password='testpass'):
+    return get_user_model().objects.create_user(email, password)
 class ModelTests(TestCase):
     
     def test_create_user_with_email_successfull(self):
@@ -39,3 +43,10 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
 
+    def test_tag_str(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
